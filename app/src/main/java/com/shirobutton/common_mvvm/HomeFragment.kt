9e9ec08a7705619d57a8303dcc31e4a3add73721
シロボタン <shirobutton@gmail.com>
2022-02-16 22:19:55 +0900

    Change HomeFragment to use Glide

--- app/src/main/java/com/shirobutton/common_mvvm/HomeFragment.kt
+++ app/src/main/java/com/shirobutton/common_mvvm/HomeFragment.kt
@@ -1,5 +1,18 @@
 package com.shirobutton.common_mvvm
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.shirobutton.common_mvvm.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment(R.layout.fragment_home)
+class HomeFragment: Fragment(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentHomeBinding.bind(view)
+        val imageView = binding.imageView
+        Glide.with(imageView)
+            .load("https://purr.objects-us-east-1.dream.io/i/tumblr_luhf8e8rwh1qaoexto1_1280.jpg")
+            .into(imageView)
+    }
+}
