88fadb48554096622247d653108107d42e52b379
シロボタン <shirobutton@gmail.com>
2022-02-19 00:13:30 +0900

    Fix HomeViewModel for coroutine

--- app/src/main/java/com/shirobutton/common_mvvm/HomeViewModel.kt
+++ app/src/main/java/com/shirobutton/common_mvvm/HomeViewModel.kt
@@ -3,6 +3,8 @@ package com.shirobutton.common_mvvm
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
     private val mutableImageUrlObservable = MutableLiveData<String>()
@@ -11,6 +13,8 @@ class HomeViewModel : ViewModel() {
     private val repository = HomeRepository()
 
     fun fetch() {
-        mutableImageUrlObservable.value = repository.getImageUrl()
+        viewModelScope.launch {
+            mutableImageUrlObservable.value = repository.getImageUrl()
+        }
     }
 }
