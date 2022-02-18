8094da08b28ec4985557733c40352f626fc9d65c
シロボタン <shirobutton@gmail.com>
2022-02-19 00:08:52 +0900

    Change HomeRepository to use RandomCatApi

--- app/src/main/java/com/shirobutton/common_mvvm/HomeRepository.kt
+++ app/src/main/java/com/shirobutton/common_mvvm/HomeRepository.kt
@@ -1,5 +1,5 @@
 package com.shirobutton.common_mvvm
 
 class HomeRepository {
-     fun getImageUrl() = "https://purr.objects-us-east-1.dream.io/i/tumblr_luhf8e8rwh1qaoexto1_1280.jpg"
+     suspend fun getImageUrl() = ApiClient.randomCatApi.getRandomCat().file
 }
