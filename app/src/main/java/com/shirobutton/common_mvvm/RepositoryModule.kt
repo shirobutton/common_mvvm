aca14ad37e8d3df178270029ed8c9895538b5146
シロボタン <shirobutton@gmail.com>
2022-02-20 14:56:49 +0900

    Add RepositoryModule

---
+++ app/src/main/java/com/shirobutton/common_mvvm/RepositoryModule.kt
@@ -0,0 +1,13 @@
+package com.shirobutton.common_mvvm
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object RepositoryModule  {
+    @Provides
+    fun provideHomeRepository() = HomeRepository()
+}
