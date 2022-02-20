760242b7442b16b9dd5f77ed89daefbee9ae0836
シロボタン <shirobutton@gmail.com>
2022-02-20 17:04:27 +0900

    Change ImageView.load to BindingAdapter

--- app/src/main/java/com/shirobutton/common_mvvm/ViewExt.kt
+++ app/src/main/java/com/shirobutton/common_mvvm/ViewExt.kt
@@ -1,8 +1,10 @@
 package com.shirobutton.common_mvvm
 
 import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
+@BindingAdapter("load")
 fun ImageView.load(url: String?) {
     Glide.with(this)
         .load(url)
